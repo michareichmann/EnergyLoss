fe6c66ae2d7fdb1736d2b0ca744e0db485f8f767
michareichmann <micha.reichmann@gmail.com>
2020-10-29 18:05:14 +0100

    first commit

---
+++ eloss.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+from root_draw import *
+
+
+def bethe_bloch(t):
+    pass
+
+
+def beta_gamma_range():
+    info('Beta Gamma Range: {:1.1f} ~ {:1.1f}'.format(beta_gamma(260, M_PI), beta_gamma(1e5, M_PI)))
+
+
+if __name__ == '__main__':
+
+    beta_gamma_range()
