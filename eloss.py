cb00c6cc33ac1d2b395018534b3fd13f1eb5f7fd
michareichmann <micha.reichmann@gmail.com>
2020-11-10 08:00:57 +0100

    adding Argon and delta-rays

--- eloss.py
+++ eloss.py
@@ -19,6 +19,10 @@ class Element(object):
         self.Density = choose(density, el.density)
         self.EPlasma = sqrt(self.Density * self.Z / self.A) * 28.816 * 1e-6  # MeV
 
+    def __repr__(self):
+        header = ['Name', 'Z', 'A [g/mol]', 'ρ [g/cm3]', 'a', 'k', 'x0', 'x1', 'I [MeV]', 'C', 'δ0', 'EPlasma [MeV]']
+        return print_table([[self.Name, self.Z, self.A, self.Density, self.A0, self.K, self.X0, self.X1, '{:.2e}'.format(self.IE), self.C, self.D0, '{:.2e}'.format(self.EPlasma)]], header, prnt=False)
+
     def draw_data(self):
         g = Draw.make_tgrapherrors(*genfromtxt(self.DataFile, usecols=[1, 2], skip_header=10).T, markersize=.7)
         g.Draw('p')
@@ -39,6 +43,7 @@ K = 4 * constants.pi * constants.Avogadro * constants.physical_constants['classi
 Si = Element(pt.silicon)
 Dia = Element(pt.carbon, density=3.52, name='Diamond')
 Cu = Element(pt.copper)
+Ar = Element(pt.argon, density=1.662e-3)
 
 Muon = Particle('Muon', constants.physical_constants['muon mass'][0], 2.1969e-6)
 Pion = Particle('Pion', 139.57018, 2.6033e-8)
@@ -55,6 +60,10 @@ def get_x(p, part: Particle):
     return log10(p / part.M)
 
 
+def offset(p, part: Particle, el: Element):
+    return K * el.Z / el.A / calc_speed(p, part.M) ** 2 * density_correction(p, part, el) / 2
+
+
 def density_correction(p, part: Particle, el: Element):
     x = get_x(p, part)
     if x >= el.X1:
@@ -65,10 +74,6 @@ def density_correction(p, part: Particle, el: Element):
         return el.D0 * 10 ** (2 * (x - el.X0))
 
 
-def offset(p, part: Particle, el: Element):
-    return K * el.Z / el.A / calc_speed(p, part.M) ** 2 * density_correction(p, part, el) / 2
-
-
 def bethe_bloch(bg, part: Particle, el: Element, dens_corr=True):
     """returns the mean energy loss of particle of momentum p and mass m in el [MeV / cm]"""
     p = part.M * bg
@@ -77,10 +82,10 @@ def bethe_bloch(bg, part: Particle, el: Element, dens_corr=True):
     return K * el.Density * el.Z / el.A / b ** 2 * (.5 * log(2 * M_E * bg ** 2 * w_max(p, part.M) / el.IE ** 2) - b ** 2 - d / 2)
 
 
-def draw_density_correction(part: Particle, el: Element, pmin=10, pmax=1e6):
-    f = Draw.make_tf1('Density Correction for {}s in {}'.format(part.Name, el.Name), density_correction, pmin, pmax, part=part, el=el)
-    Draw.histo(f, logx=True, logy=True)
-    format_histo(f, x_tit='#beta#gamma', y_tit='Value', y_off=1.1, center_y=True, center_x=True)
+def delta_rays(wmin, dx, p, part: Particle, el: Element):
+    b = calc_speed(p, part.M)
+    wmax = w_max(p, part.M)
+    return .5 * K * el.Z / el.A / b ** 2 * ((1 / wmin - 1 / wmax) - b ** 2 / wmax * log(wmax / wmin)) * dx * el.Density
 
 
 def get_minimum(p: Particle = Pion, el: Element = Dia):
@@ -108,6 +113,12 @@ def get_fall_coeff(p: Particle = Pion, el: Element = Dia):
     info('Fall coefficient for {}s in {}: {:1.2f} ({:1.2f})'.format(p.Name, el.Name, fit.GetParameter(2), fit.GetParError(2)))
 
 
+def draw_density_correction(part: Particle, el: Element, pmin=10, pmax=1e6):
+    f = Draw.make_tf1('Density Correction for {}s in {}'.format(part.Name, el.Name), density_correction, pmin, pmax, part=part, el=el)
+    Draw.histo(f, logx=True, logy=True)
+    format_histo(f, x_tit='#beta#gamma', y_tit='Value', y_off=1.1, center_y=True, center_x=True)
+
+
 def draw_bethe(part: Particle, el: Element, bg_min=1, bg_max=1e3, draw_data=False, y_range=None):
     f = Draw.make_tf1('', bethe_bloch, bg_min, bg_max, part=part, el=el)
     f0 = Draw.make_tf1('Bethe-Bloch for {}s in {}'.format(part.Name, el.Name), bethe_bloch, bg_min, bg_max, 4, part=part, el=el, dens_corr=False)
@@ -124,6 +135,12 @@ def draw_bethe(part: Particle, el: Element, bg_min=1, bg_max=1e3, draw_data=Fals
         g = el.draw_data()
         leg.AddEntry(g, 'TCut = 0.05 MeV', 'p')
     return f
+
+
+def draw_drays(p, dx, part: Particle, el: Element, wmin=1e-3, y_range=[1e-5, 10]):
+    f = Draw.make_tf1('Delta Rays for {}s in {} cm {}'.format(part.Name, dx, el.Name), delta_rays, wmin, w_max(p, part.M), dx=dx, p=p, part=part, el=el)
+    Draw.histo(f, logx=True, logy=True, lm=.13, bm=.22)
+    format_histo(f, x_tit='T_{min} [MeV]', y_tit='N_{#delta}', y_off=1.3, center_y=True, center_x=True, x_off=1.4, y_range=y_range, tit_size=.05, lab_size=.045)
 
 
 def beta_gamma_range():
