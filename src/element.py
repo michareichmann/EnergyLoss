f74a0141ed0f06d63783fea14d5b9fe64e5f2a0e
michareichmann <micha.reichmann@gmail.com>
2020-11-19 15:40:41 +0100

    add methods to draw mnuon data

--- src/element.py
+++ src/element.py
@@ -5,12 +5,13 @@ from src.particle import Muon
 
 
 class Element(object):
-    def __init__(self, el: pt.core.Element, e_eh=1., density=None, name=None):
+    def __init__(self, el: pt.core.Element, rad_length, e_eh=1., density=None, name=None):
         self.Name = choose(name, el.name.title())
         self.Z = el.number
         self.A = el.mass
         self.DataFile = join(dirname(dirname(realpath(__file__))), 'data', '{}.txt'.format(self.Name))
-        self.A0, self.K, self.X0, self.X1, self.IE, self.C, self.D0 = genfromtxt(self.DataFile, skip_header=4, max_rows=1)
+        self.X0 = rad_length
+        self.a, self.k, self.x0, self.x1, self.IE, self.C, self.D0 = genfromtxt(self.DataFile, skip_header=4, max_rows=1)
         self.IE *= 1e-6  # convert to MeV
         self.Density = choose(density, el.density)
         self.EPlasma = sqrt(self.Density * self.Z / self.A) * 28.816 * 1e-6  # MeV
@@ -18,18 +19,40 @@ class Element(object):
 
     def __repr__(self):
         header = ['Name', 'Z', 'A [g/mol]', 'ρ [g/cm3]', 'a', 'k', 'x0', 'x1', 'I [MeV]', 'C', 'δ0', 'EPlasma [MeV]']
-        return print_table([[self.Name, self.Z, self.A, self.Density, self.A0, self.K, self.X0, self.X1, '{:.2e}'.format(self.IE), self.C, self.D0, '{:.2e}'.format(self.EPlasma)]], header, prnt=False)
+        return print_table([[self.Name, self.Z, self.A, self.Density, self.a, self.k, self.x0, self.x1, '{:.2e}'.format(self.IE), self.C, self.D0, '{:.2e}'.format(self.EPlasma)]], header, prnt=False)
 
-    def get_data(self, linear=True, mass=False, t=500):
-        x, y = genfromtxt(self.DataFile, usecols=[1, 2], skip_header=10).T
+    def get_data(self, col, linear, mass, t):
+        x, y = genfromtxt(self.DataFile, usecols=[0, col], skip_header=10).T
         return array([beta_gamma(x, Muon.M), y * (1 if mass else self.Density if linear else t * self.Density)])
 
-    def draw_data(self, linear=True, mass=False, t=500):
-        Draw.make_tgrapherrors(*self.get_data(linear, mass, t), markersize=.7).Draw('p')
+    def get_ionisation(self, linear=True, mass=False, t=500):
+        return self.get_data(2, linear, mass, t)
+
+    def get_radlos(self, linear=True, mass=False, t=500):
+        return self.get_data(6, linear, mass, t)
+
+    def get_brems(self, linear=True, mass=False, t=500):
+        return self.get_data(3, linear, mass, t)
+
+    def draw_ionisation(self, linear=True, mass=False, t=500):
+        Draw.make_tgrapherrors(*self.get_ionisation(linear, mass, t), markersize=.7).Draw('p')
+
+    def draw_radloss(self, linear=True, mass=False, t=500):
+        Draw.make_tgrapherrors(*self.get_radlos(linear, mass, t), markersize=.7).Draw('p')
+
+    def draw_full_radloss(self, n=-1):
+        d = Draw()
+        x, y = self.get_radlos(mass=True)
+        d.graph(x[:n], y[:n], logx=True)
+
+    def draw_full_brems(self, n=-1):
+        d = Draw()
+        x, y = self.get_brems(mass=True)
+        d.graph(x[:n], y[:n], logx=True)
 
 
-Si = Element(pt.silicon, e_eh=3.68)
-Dia = Element(pt.carbon, e_eh=13.3, density=3.52, name='Diamond')
-Cu = Element(pt.copper)
-Ar = Element(pt.argon, density=1.662e-3)
-Pb = Element(pt.lead)
+Si = Element(pt.silicon, 21.82, e_eh=3.68)
+Dia = Element(pt.carbon, 42.70, e_eh=13.3, density=3.52, name='Diamond')
+Cu = Element(pt.copper, 12.86)
+Ar = Element(pt.argon, 19.55, density=1.662e-3)
+Pb = Element(pt.lead, 6.37)
