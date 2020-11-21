25932ad42bb78758ed9bed5c7a0b7a92c07d1ef0
michareichmann <micha.reichmann@gmail.com>
2020-11-21 11:24:39 +0100

    revise draw_ep() and add print_energies() and get_losses()

--- main.py
+++ main.py
@@ -36,14 +36,30 @@ def draw_bethe_mpv(p=Pion, el=Dia, t=500, w_cut=2, y_range=None):
     Draw.legend([f.F for f in fs], ['Bethe Bloch', 'W_{{cut}}={}dE/dx_{{min}}'.format(w_cut), 'Landau-Vavilov-Bichsel'], 'l', x2=.89, w=.33, scale=1.4)
 
 
-def draw_ep(el=Dia, t=500, absolute=False, mass=False, y_range=None, xmin=.1, xmax=1e3):
-    bethe_el = BetheBloch(Electron, el, t, absolute=absolute, mass=mass).draw(y_range=y_range, xmin=xmin, xmax=xmax)
-    bethe_pos = BetheBloch(Positron, el, t, absolute=absolute, mass=mass).draw_same(4, 2)
-    brems = Bremsstrahlung(Electron, el, t, absolute, mass).draw_same(3)
-    total = (bethe_el + brems).draw_same()
-    data = Cu.draw_brems(not absolute, mass, t=t / 10)
-    Draw.legend([bethe_el.F, bethe_pos.F, total.F, brems.F, data], ['Ionisation e^{-}', 'Ionisation e^{+}', 'total', 'Bremsstrahlung appr.', 'Bremstrahlung exact'], 'l')
+def draw_ep(el=Dia, t=500, absolute=False, mass=False, logy=False, y_range=None, xmin=.1, xmax=1e3):
+    bethe_el = BetheBloch(Electron, el, t, absolute=absolute, mass=mass).draw(y_range=y_range, xmin=xmin, xmax=xmax, logy=logy, color=Draw.get_colors(10)[0])
+    bethe_pos = BetheBloch(Positron, el, t, absolute=absolute, mass=mass).draw_same(Draw.get_colors(10)[0], 2)
+    brems = Bremsstrahlung(Electron, el, t, absolute, mass).draw_same(Draw.get_colors(10)[7], 10)
+    data = el.draw_brems(not absolute, mass, t=t / 10, color=Draw.get_colors(10)[9])
+    total = (bethe_el + data).draw_same(1)
+    Draw.legend([bethe_el.F, bethe_pos.F, total.F, brems.F, data], ['Ionisation e^{-}', 'Ionisation e^{+}', 'total Eloss', 'Bremsstrahlung appr.', 'Bremstrahlung exact'], 'l')
+    return total
 
 
 def beta_gamma_range():
     info('Beta Gamma Range: {:1.1f} ~ {:1.1f}'.format(beta_gamma(260, M_PI), beta_gamma(1.2e5, M_PI)))
+
+
+def get_losses(p, el, t=500):
+    return array([BetheBloch(part, el, t, absolute=True)(p) for part in [Pion, Muon, Positron, Proton]])
+
+
+def print_energies(p, el: Element, t=500, table=False):
+    particles = [Pion, Muon, Positron, Proton]
+    values = []
+    for part in particles:
+        e = BetheBloch(part, el, t, absolute=True)(p)
+        values.append(['{:1.0f}'.format(e), '{:1.0f}'.format(round(e * 1000 / el.EEH, -2))])
+        info('Energy loss of {} MeV {}s in {} μm {}: {:1.0f} keV ({:1.0f} eh-pairs)'.format(p, part.Name, t, el.Name, e, round(e * 1000 / el.EEH, -2)))
+    if table:
+        print(make_latex_table_row(concatenate(array(values).T)))
