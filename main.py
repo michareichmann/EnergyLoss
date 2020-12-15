570c270bae240b85876972fc98e041275ad2df0e
michareichmann <micha.reichmann@gmail.com>
2020-12-15 09:17:49 +0100

    renaming

--- main.py
+++ main.py
@@ -3,13 +3,13 @@
 from src.eloss import *
 from src.straggling import *
 
-draw = Draw(join(Dir, 'main.ini'))
+ddraw = Draw(join(Dir, 'main.ini'))
 
 
 def draw_dens_correction(p, el, y_range=None):
     f, f0 = BetheBloch(p, el), BetheBloch(p, el, dens_corr=False)
-    f0.draw(color=draw.get_color(2, 1), line_style=7, y_range=y_range)
-    f.draw_same(draw.get_color(2, 0))
+    f0.draw(color=ddraw.get_color(2, 1), line_style=7, y_range=y_range)
+    f.draw_same(ddraw.get_color(2, 0))
     Draw.legend([f.F, f0.F], ['Bethe Bloch', 'without #delta'], 'l', x2=.96, w=.2, scale=1.4)
 
 
@@ -17,10 +17,10 @@ def draw_restricted(p, el, w_cut: Any = 2, t=500, abst=False, mass=False, y_rang
     b0 = BetheBloch(p, el, abst=abst, mass=mass)
     n = 2 + make_list(w_cut).size
     fs = [BetheBloch(p, el, t, d, w, abst, mass) for d, w in [(1, None), (0, None)] + [(1, iw) for iw in make_list(w_cut) * b0.get_minimum()[0]]]
-    fs[1].draw(color=draw.get_color(n, n - 1), line_style=7, y_range=y_range)
+    fs[1].draw(color=ddraw.get_color(n, n - 1), line_style=7, y_range=y_range)
     for i, f in enumerate(fs[2:], 1):
-        f.draw_same(draw.get_color(n, i), 9)
-    fs[0].draw_same(draw.get_color(n, 0))
+        f.draw_same(ddraw.get_color(n, i), 9)
+    fs[0].draw_same(ddraw.get_color(n, 0))
     Draw.legend([f.F for f in fs], ['Bethe Bloch', 'without #delta'] + ['W_{{cut}}={}dE/dx_{{min}}'.format(i) for i in make_list(w_cut)], 'l', x2=.96, w=.23, scale=1.4)
 
 
@@ -31,9 +31,9 @@ def draw_bethe(p, el, t=500, abst=False, mass=False, y_range=None):
 def draw_bethe_mpv(p=Pion, el=Dia, t=500, w_cut=2, y_range=None):
     b0 = BetheBloch(p, el)
     fs = [BetheBloch(p, el, t, abst=True), BetheBloch(p, el, t, abst=True, wcut=w_cut * b0.get_minimum()[0]), LandauVavilovBichsel(p, el, t, True)]
-    fs[1].draw(color=draw.get_color(3, 2), line_style=9, y_range=y_range)
-    fs[0].draw_same(draw.get_color(3, 0))
-    fs[2].draw_same(draw.get_color(5, 1), line_style=6)
+    fs[1].draw(color=ddraw.get_color(3, 2), line_style=9, y_range=y_range)
+    fs[0].draw_same(ddraw.get_color(3, 0))
+    fs[2].draw_same(ddraw.get_color(5, 1), line_style=6)
     Draw.legend([f.F for f in fs], ['Bethe Bloch', 'W_{{cut}}={}dE/dx_{{min}}'.format(w_cut), 'Landau-Vavilov-Bichsel'], 'l', x2=.89, w=.33, scale=1.4)
 
 
@@ -73,5 +73,6 @@ def draw_straggling(part=Pion, el=Dia, p=260, t=500, n=1e6, bin_size=1):
 
 
 if __name__ == '__main__':
-    z = Straggling(Pion, Si)
-    a = Landau(Pion, Dia, 260, 500)
+    zd = Straggling(Pion, Dia, 260, 500)
+    zs = Straggling(Pion, Si, 260, 300)
+    z = Straggling(Electron, Si, 10e3, 148)
